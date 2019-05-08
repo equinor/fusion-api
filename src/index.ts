8e7c61e0f18535caa1eba722eac56dea0bbb9fcd
Martin Førre <martin.forre@bouvet.no>
2019-05-08 17:09:32 +0200

    Add app context and router

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export {
-    IAuthContainer,
-    default as AuthContainer
-} from "./auth/AuthContainer";
+export { IAuthContainer, default as AuthContainer } from "./auth/AuthContainer";
+
+export { registerApp, registerAppListener } from "./app/AppRegistrationContainer";
+
+export { default as AppContext, IAppContext } from "./app/AppContext";
+
+export { AppRoute, AppLink, AppSwitch } from "./app/AppRouter";
