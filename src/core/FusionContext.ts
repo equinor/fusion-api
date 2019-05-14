69bdfd90e5210e2c7ceec2d4c76c3ad3680358a3
Martin Førre <martin.forre@bouvet.no>
2019-05-14 10:34:26 +0200

    Add refs to fusion context

--- src/core/FusionContext.ts
+++ src/core/FusionContext.ts
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, MutableRefObject } from "react";
 import { IAuthContainer } from "../auth/AuthContainer";
 import ResourceCollections from "../http/resourceCollections";
 import ApiClients from "../http/apiClients";
@@ -12,9 +12,15 @@ type Http = {
     apiClients: ApiClients;
 };
 
+type Refs = {
+    root: MutableRefObject<HTMLElement>;
+    overlay: MutableRefObject<HTMLElement>;
+};
+
 export interface IFusionContext {
     auth: Auth;
     http: Http;
+    refs: Refs;
 }
 
 const FusionContext = createContext<IFusionContext>({} as IFusionContext);
