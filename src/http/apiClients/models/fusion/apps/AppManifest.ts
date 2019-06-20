f7be0262c597f94440f5424dedd0b79f6d27db4c
Martin Førre <martin.forre@bouvet.no>
2019-06-20 11:45:57 +0200

    Add context types and app auth to app manifest

--- src/http/apiClients/models/fusion/apps/AppManifest.ts
+++ src/http/apiClients/models/fusion/apps/AppManifest.ts
@@ -1,5 +1,13 @@
-type AppManifest = {
+import { ContextTypes } from '../../context';
 
+export type AppAuth = {
+    clientId: string;
+    resources: string[];
+};
+
+type AppManifest = {
+    contextTypes?: ContextTypes[];
+    auth?: AppAuth;
 }
 
 export default AppManifest;
