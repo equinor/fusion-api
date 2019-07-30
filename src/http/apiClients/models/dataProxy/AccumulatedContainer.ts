1ece54f437fdf2e113d544903341576ffaa2541f
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2019-07-30 10:46:11 +0200

    Added startDate to accumulated container model

--- src/http/apiClients/models/dataProxy/AccumulatedContainer.ts
+++ src/http/apiClients/models/dataProxy/AccumulatedContainer.ts
@@ -2,6 +2,7 @@ export type AccumulatedContainer = {
     siteCode: string,
     projectIdentifier: string,
     totalCount: number,
+    startDate?: string,
     dataQualityCount: number,
     dataQuality: string[],
     accumulated: string[]
