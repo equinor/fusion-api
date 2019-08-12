f7f8c1bf9c5f77e75b1d1bb97b4ac9b0fa3763a6
Martin Førre <martin.forre@bouvet.no>
2019-08-12 16:21:16 +0200

    Date time parsing and masks

--- src/intl/DateTime.ts
+++ src/intl/DateTime.ts
@@ -22,27 +22,51 @@ const weekDayFormatter = new Intl.DateTimeFormat(locale, { weekday: 'short' });
 
 const dayFormatter = new Intl.DateTimeFormat(locale, { day: '2-digit' });
 
-export const formatDateTime = (date: Date | string | number) => {
+export const formatDateTime = (date: Date | number) => {
     const parsedDate = new Date(date);
     return dateTimeFormatter.format(parsedDate);
 };
 
-export const formatDate = (date: Date | string | number) => {
+export const formatDate = (date: Date | number) => {
     const parsedDate = new Date(date);
     return dateFormatter.format(parsedDate);
 };
 
-export const formatTime = (date: Date | string | number) => {
+export const formatTime = (date: Date | number) => {
     const parsedDate = new Date(date);
     return timeFormatter.format(parsedDate);
 };
 
-export const formatWeekDay = (date: Date | string | number) => {
+export const formatWeekDay = (date: Date | number) => {
     const parsedDate = new Date(date);
     return weekDayFormatter.format(parsedDate);
 };
 
-export const formatDay = (date: Date | string | number) => {
+export const formatDay = (date: Date | number) => {
     const parsedDate = new Date(date);
     return dayFormatter.format(parsedDate);
 };
+
+/**
+ * Parse a date string to Date
+ * @param dateString Expected format: dd/mm/yyyy
+ */
+export const parseDate = (dateString: string) => {
+    const parts = dateString.split('/').map(part => parseInt(part, 10));
+    return new Date(parts[2], parts[1] - 1, parts[0]);
+};
+
+/**
+ * Parse a date time string to Date
+ * @param dateTimeString Expected format: dd/mm/yyyy, hh:mm
+ */
+export const parseDateTime = (dateTimeString: string) => {
+    const parts = dateTimeString.split(', ');
+    const dateParts = parts[0].split('/').map(part => parseInt(part, 10));
+    const timeParts = parts[1].split(':').map(part => parseInt(part, 10));
+    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0], timeParts[0], timeParts[1]);
+};
+
+export const dateMask = '39/19/9999';
+export const timeMask = '29:69';
+export const dateTimeMask = `${dateMask}, ${timeMask}`;
