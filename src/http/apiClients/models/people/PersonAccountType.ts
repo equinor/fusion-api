54e43f9b7556ff0b6b0471153a26da113653fe2b
Morten Salte <morten.salte@bouvet.no>
2019-08-26 12:38:57 +0200

    Added local as an account type for completion

--- src/http/apiClients/models/people/PersonAccountType.ts
+++ src/http/apiClients/models/people/PersonAccountType.ts
@@ -1,3 +1,8 @@
-type PersonAccountType = 'consultant' | 'employee' | 'external';
+/// Employee has a corporate mail address and is identified by user having membership in the F.[Company] AD group.
+/// Consultants has a corporate mail address and membership in the K.[Company] AD group.
+/// External users has a GUEST account in azure ad, AKA affiliate access account.
+/// Local users only exists in the fusion database. - These users can not log in.
+
+type PersonAccountType = 'consultant' | 'employee' | 'external' | 'local';
 
 export default PersonAccountType;
