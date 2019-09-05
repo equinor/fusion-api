4eee6ea720dd9e7e2f6827e4576aaa3c2b9736e6
Martin Førre <martin.forre@bouvet.no>
2019-09-05 13:49:06 +0200

    Force update history on history change

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { History } from "history";
 import { useFusionContext } from "../core/FusionContext";
 
@@ -10,6 +10,14 @@ export const HistoryContext = createContext<IHistoryContext>({ history: null });
 export default (): History => {
     const fusionContext = useFusionContext();
     const historyContext = useContext(HistoryContext);
+
+    const history = historyContext.history || fusionContext.history;
+
+    const [_, forceUpdate] = useState(null);
+    useEffect(() => {
+        const listner = history.listen(() => forceUpdate(null));
+        return listner;
+    }, [history]);
 
     return historyContext.history || fusionContext.history;
 };
