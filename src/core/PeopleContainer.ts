756d1ee58c79784d01ef3201323b298cd5702274
Morten Salte <morten.salte@bouvet.no>
2019-09-12 13:02:28 +0200

    Caching image both onerror and onload

--- src/core/PeopleContainer.ts
+++ src/core/PeopleContainer.ts
@@ -71,11 +71,9 @@ export default class PeopleContainer {
             const image = new Image();
             image.src = urlToImage;
 
+            this.images[personId] = image;
             image.onerror = () => reject(`Could not load image ${urlToImage}.`);
-            image.onload = () => {
-                this.images[personId] = image;
-                resolve(image);
-            };
+            image.onload = () => resolve(image);
         });
     }
 }
