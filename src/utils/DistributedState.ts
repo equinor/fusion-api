001104f1773941922494da21ddc413af67d27c74
Ole-Richard Blindheim <oblin@equinor>
2019-11-01 14:48:00 +0100

    Implement distributed state in Notification center. InitialState distribution of distributed state

--- src/utils/DistributedState.ts
+++ src/utils/DistributedState.ts
@@ -7,19 +7,22 @@ export interface IDistributedState<T> extends IEventEmitter<DistributedStateEven
 
 export type DistributedStateEvents<T> = {
     change: (state: T) => void;
-}
+};
 
-class DistributedState<T> extends EventEmitter<DistributedStateEvents<T>> implements IDistributedState<T> {
+class DistributedState<T> extends EventEmitter<DistributedStateEvents<T>>
+    implements IDistributedState<T> {
     private _state: T;
     private readonly _key: string;
     private readonly _eventHub: IEventHub;
 
-    constructor(key: string, state: T, evenHub: IEventHub) {
+    constructor(key: string, state: T, eventHub: IEventHub) {
         super();
         this._key = key;
         this._state = state;
-        this._eventHub = evenHub;
-        evenHub.registerListener<T>(key, this.handleUpdatedState);
+        this._eventHub = eventHub;
+        eventHub.registerListener<T>(key, this.handleUpdatedState);
+        eventHub.publish(key + 'InitialState', void null);
+        eventHub.registerListener<void>(key + 'InitialState', this.handleNewInstance);
     }
 
     get state() {
@@ -34,7 +37,11 @@ class DistributedState<T> extends EventEmitter<DistributedStateEvents<T>> implem
     private handleUpdatedState = (state: T) => {
         this._state = state;
         this.emit('change', this._state);
-    }
+    };
+
+    private handleNewInstance = () => {
+        this._eventHub.publish(this._key, this._state);
+    };
 }
 
 export default DistributedState;
