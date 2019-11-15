44521104bb70eb55a7e12c50530462a01a31ba26
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2019-11-15 12:46:55 +0100

    Moved abortcontrollermanager

--- src/core/FusionContext.ts
+++ src/core/FusionContext.ts
@@ -129,10 +129,11 @@ export const createFusionContext = (
         options && options.telemetry ? options.telemetry.instrumentationKey : '',
         authContainer
     );
-    const abortControllerManager = new AbortControllerManager(new EventHub());
     const resourceCollections = createResourceCollections(serviceResolver, options);
-
+    
     const resourceCache = new ResourceCache(new EventHub());
+    const abortControllerManager = new AbortControllerManager(new EventHub());
+    
     const httpClient = new HttpClient(
         authContainer,
         resourceCache,
