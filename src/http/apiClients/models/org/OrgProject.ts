4ef0c048e46845c9d75c06078392f8742640a612
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2019-12-12 08:22:37 +0100

    Updated org project model and api versioning for project search

--- src/http/apiClients/models/org/OrgProject.ts
+++ src/http/apiClients/models/org/OrgProject.ts
@@ -9,8 +9,14 @@ import CreateOrgProject from './CreateOrgProject';
 
 type OrgProject = {
     dates: {
-        endDate: Date;
-        startDate: Date;
+        endDate: Date | null;
+        startDate: Date | null;
+        gates: {
+            dG1: Date | null,
+            dG2: Date | null,
+            dG3: Date | null,
+            dG4: Date | null,
+        }
     };
     director: Position;
     directorPositionId: string;
