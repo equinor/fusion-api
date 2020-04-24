8be0efa22c7c7eb2618099212c03b8ce2a87007b
Ole-Richard Blindheim <post@olerichard.org>
2020-04-24 09:17:48 +0200

    Added modelfiles for milestones and workorders

---
+++ src/http/apiClients/models/dataProxy/workorder/WorkOrderMccr.ts
@@ -0,0 +1,14 @@
+export type WorkOrderMccr = {
+    workOrderId: string;
+    tagNumber: string;
+    description: string;
+    tagUrl: string;
+    mccrType: string;
+    mccrStatus: string;
+    mccrResponsible: string;
+    mccrUrl: string;
+    mcpkgNumber: string;
+    commpkgId: string;
+    commpkgNumber: string;
+    commpkgUrl: string;
+};
