447fe55cb7789c2d2e1277f55f1f9dc2ac0b5b9d
Ole-Richard Blindheim <post@olerichard.org>
2020-04-24 08:55:31 +0200

    added workorders and milestone endpoint to dataproxyClient

--- src/http/apiClients/DataProxyClient.ts
+++ src/http/apiClients/DataProxyClient.ts
@@ -12,6 +12,10 @@ import {
     HandoverWorkOrder,
     HandoverQuery,
     AccumulatedContainer,
+    WorkOrder,
+    WorkOrderMaterial,
+    WorkOrderMccr,
+    Milestone,
 } from './models/dataProxy';
 import {
     HandoverActions,
@@ -40,7 +44,7 @@ export default class DataProxyClient extends BaseApiClient {
     protected getBaseUrl() {
         return this.serviceResolver.getDataProxyBaseUrl();
     }
-    
+
     async getHandoverAsync(siteCode: string, projectIdentifier: string) {
         const url = this.resourceCollections.dataProxy.handover(siteCode, projectIdentifier);
         return await this.httpClient.getAsync<HandoverItem[], FusionApiHttpErrorResponse>(url);
@@ -71,5 +75,29 @@ export default class DataProxyClient extends BaseApiClient {
             action
         );
         return await this.httpClient.getAsync<T[], FusionApiHttpErrorResponse>(url);
+    }
+
+    async getMilestonesAsync(contextId: string) {
+        const url = this.resourceCollections.dataProxy.milestones(contextId);
+        return await this.httpClient.getAsync<Milestone[], FusionApiHttpErrorResponse>(url);
+    }
+
+    async getWorkOrdersAsync(contextId: string, invalidateCache: boolean) {
+        const url = this.resourceCollections.dataProxy.workOrders(contextId);
+        const options = invalidateCache ? { headers: { 'x-pp-cache-policy': 'no-cache' } } : {};
+        return await this.httpClient.getAsync<WorkOrder[], FusionApiHttpErrorResponse>(
+            url,
+            options
+        );
+    }
+
+    async getWorkOrderMaterialsAsync(contextId: string, workOrderId: string) {
+        const url = this.resourceCollections.dataProxy.workOrdersMaterials(contextId, workOrderId);
+        return await this.httpClient.getAsync<WorkOrderMaterial[], FusionApiHttpErrorResponse>(url);
+    }
+
+    async getWorkOrderMccrAsync(contextId: string, workOrderId: string) {
+        const url = this.resourceCollections.dataProxy.workOrdersMccr(contextId, workOrderId);
+        return await this.httpClient.getAsync<WorkOrderMccr[], FusionApiHttpErrorResponse>(url);
     }
 }
