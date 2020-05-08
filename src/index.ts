d055fe11516df764d62225cb4771fe512fb4fb3e
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-05-08 10:34:59 +0200

    Export

--- src/index.ts
+++ src/index.ts
@@ -169,6 +169,7 @@ export {
     RegisterNotificationPresenter,
     INotificationContext,
     useNotificationCards,
+    useNotificationCardActions,
 } from './core/NotificationCenter';
 
 export {
