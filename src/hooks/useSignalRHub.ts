43c0a66a96ea67d6514584b5d02d8cd17375d1b5
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-05-11 11:32:10 +0200

    Debugging

--- src/hooks/useSignalRHub.ts
+++ src/hooks/useSignalRHub.ts
@@ -19,6 +19,7 @@ export default (hubName: string) => {
             setHubDetails(response.data);
         } catch (e) {
             setHubConnectionError(e);
+            console.error(e);
         }
     }, [apiClients]);
 
@@ -36,6 +37,7 @@ export default (hubName: string) => {
             setHubConnection(hubConnect);
         } catch (e) {
             setHubConnectionError(e);
+            console.error(e);
         } finally {
             setIsEstablishingHubConnection(false);
         }
