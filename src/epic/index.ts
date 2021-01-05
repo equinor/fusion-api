73c36a036b88c5573a79f470dd4ce14a7110d221
odin thomas rochmann <odin.rochmann@gmail.com>
2021-01-05 16:51:15 +0100

    feat: add observable state

--- src/epic/index.ts
+++ src/epic/index.ts
@@ -7,3 +7,4 @@ export * from './StatefulObserver';
 export * from './hooks/useEpic';
 export * from './hooks/useSelector';
 export * from './hooks/useObservableSelector';
+export * from './hooks/useObservableState';
