eb6af4ac070fd92e16de4f1c33a576a9e016b257
odin thomas rochmann <odin.rochmann@gmail.com>
2021-01-08 08:59:47 +0100

    fix: url sanitize
    - remove positive lookbehind

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,14 +1,12 @@
-const separator = '/';
-
 export const trimTrailingSlash = (url: string) => url.replace(/\/$/, '');
 
 export const sanitizedUrl = (url: string) =>
-    trimTrailingSlash(url.replace(/(?<=[^:\s])(\/+\/)/g, separator));
+    trimTrailingSlash(url.replace(/(\w:\/\/)|(\/)+/g, '$1$2'));
 
 export const combineUrls = (base: string, ...parts: string[]) =>
     sanitizedUrl(
         [base]
             .concat(parts)
             .filter((a) => !!a)
-            .join(separator)
+            .join('/')
     );
