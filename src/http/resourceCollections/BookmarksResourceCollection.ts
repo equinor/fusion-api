27c9953adc8af2b626a0a06e03d2bbfe0d3a7e7e
Joachim <joachim290@outlook.com>
2021-04-22 07:49:10 +0200

    feat: add bookmark resource collection

--- src/http/resourceCollections/BookmarksResourceCollection.ts
+++ src/http/resourceCollections/BookmarksResourceCollection.ts
@@ -1,5 +1,6 @@
 import BaseResourceCollection from './BaseResourceCollection';
 import { combineUrls } from '../../utils/url';
+import buildQuery from 'odata-query';
 
 export default class BookmarksResourceCollection extends BaseResourceCollection {
     protected getBaseUrl(): string {
@@ -9,8 +10,22 @@ export default class BookmarksResourceCollection extends BaseResourceCollection 
     bookmark(bookmarkId: string): string {
         return combineUrls(this.getBaseUrl(), 'bookmarks', bookmarkId);
     }
+    bookmarks(): string {
+        return combineUrls(this.getBaseUrl(), 'persons', 'me', 'bookmarks');
+    }
 
     addBookmark(): string {
         return combineUrls(this.getBaseUrl(), 'bookmarks');
+    }
+
+    queryBookmarks(appKey: string): string {
+        const baseUrl = this.bookmarks();
+
+        const odataQuery = buildQuery({
+            filter: {
+                appKey: appKey,
+            },
+        });
+        return `${baseUrl}${odataQuery}`;
     }
 }
