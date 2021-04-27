587fb25fc5f9b46d496444a817ffcd26fd25ceb1
Joachim <joachim290@outlook.com>
2021-04-27 22:42:42 +0200

    feat: add api version

--- src/http/apiClients/BookmarksClient.ts
+++ src/http/apiClients/BookmarksClient.ts
@@ -10,50 +10,41 @@ export class BookmarksClient extends BaseApiClient {
     }
 
     async getBookmark(bookmarkId: string) {
-        const url = this.resourceCollections.bookmarks.bookmark(bookmarkId);
+        const baseUrl = this.resourceCollections.bookmarks.bookmark(bookmarkId);
+        const url = `${baseUrl}?api-version=1.0-preview`;
         return await this.httpClient.getAsync<BookmarkResponse, FusionApiHttpErrorResponse>(url);
     }
     async getBookmarks(appKey: string) {
-        const url = this.resourceCollections.bookmarks.queryBookmarks(appKey);
+        const baseUrl = this.resourceCollections.bookmarks.queryBookmarks(appKey);
+        const url = `${baseUrl}&api-version=1.0-preview`;
         return await this.httpClient.getAsync<BookmarkListResponse[], FusionApiHttpErrorResponse>(
-            url,
-            {
-                headers: {
-                    'api-version': '1.0-preview',
-                },
-            }
+            url
         );
     }
 
     async addBookmark(bookmark: BookmarkRequest) {
-        const url = this.resourceCollections.bookmarks.addBookmark();
+        const baseUrl = this.resourceCollections.bookmarks.addBookmark();
+        const url = `${baseUrl}?api-version=1.0-preview`;
         return await this.httpClient.postAsync<
             BookmarkRequest,
             BookmarkResponse,
             FusionApiHttpErrorResponse
-        >(url, bookmark, {
-            headers: {
-                'api-version': '1.0-preview',
-            },
-        });
+        >(url, bookmark);
     }
 
     async updateBookmark(bookmarkId: string, bookmark: Partial<BookmarkRequest>) {
-        const url = this.resourceCollections.bookmarks.updateBookmark(bookmarkId);
-
+        const baseUrl = this.resourceCollections.bookmarks.updateBookmark(bookmarkId);
+        const url = `${baseUrl}?api-version=1.0-preview`;
         return await this.httpClient.patchAsync<
             Partial<Omit<BookmarkRequest, 'appKey' | 'contextId'>>,
             BookmarkResponse,
             FusionApiHttpErrorResponse
-        >(url, bookmark, {
-            headers: {
-                'api-version': '1.0-preview',
-            },
-        });
+        >(url, bookmark);
     }
 
     async deleteBookmark(bookmarkId: string) {
-        const url = this.resourceCollections.bookmarks.deleteBookmark(bookmarkId);
+        const baseUrl = this.resourceCollections.bookmarks.deleteBookmark(bookmarkId);
+        const url = `${baseUrl}?api-version=1.0-preview`;
         return await this.httpClient.deleteAsync<void, FusionApiHttpErrorResponse>(url);
     }
 }
