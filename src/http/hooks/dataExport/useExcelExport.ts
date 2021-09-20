11cc0383a2a37d69911a70d5ad950f170c9eadb4
Joachim <joachim290@outlook.com>
2021-09-20 12:36:09 +0200

    feat: hook for downloading a generated excel document

--- src/http/hooks/dataExport/useExcelExport.ts
+++ src/http/hooks/dataExport/useExcelExport.ts
@@ -1,16 +1,35 @@
 import useApiClients from '../useApiClients';
 import { useCallback, useEffect, useState } from 'react';
+import { Sheet } from '../../../http/apiClients/models/dataExport/Sheet';
+import { useFusionContext } from '../../../core/FusionContext';
 
-type Sheet = {
-    columns: {
-        name: string;
-        type: string;
-    }[];
-    rows: string[][];
-};
-
+/**
+ * A hook that will return a client function that can be used to create Excel documents.
+ * The hook will send a `POST` request to the data export API and receive a URL where the generated
+ * Excel file can be downloaded. After receiving the URL, an anchor tag will be added and the download will start.
+ * @param props Name of the file and the name of the Excel sheet
+ * @returns A client that accepts the table data as parameters and send a request to the data export API
+ * @example
+ * import {Table, Toolbar} from "@equinor/fusion-react-table"
+ * import {useExcelExport} from "@equinor/fusion"
+ * const App = () => {
+ *  const {client} = useExcelExport({fileName: 'test', dataSetName: 'test-set'});
+ *
+ *  return (
+ *      <Table
+            options={{ columns: columns, data: data }}
+            slots={{ Toolbar: <Toolbar excel={{ client }} /> }}
+        />
+ *
+ *  )
+ * }
+ *
+ */
 export const useTableExport = (props: { fileName: string; dataSetName: string }) => {
     const { dataExport } = useApiClients();
+    const {
+        http: { resourceCollections },
+    } = useFusionContext();
     const [url, setUrl] = useState<string>('');
     const { fileName, dataSetName } = props;
 
@@ -24,7 +43,7 @@ export const useTableExport = (props: { fileName: string; dataSetName: string })
                     dataSetName,
                     sheets: data.sheets,
                 });
-                setUrl(`/api/data-exports/${tempId}.xlsx`);
+                setUrl(`${resourceCollections.dataExport.download(tempId)}.xlsx`);
             } catch (err) {
                 console.error(err);
             }
@@ -34,10 +53,9 @@ export const useTableExport = (props: { fileName: string; dataSetName: string })
 
     useEffect(() => {
         if (!url) return;
-
         const a = document.createElement('a');
         a.style.display = 'none';
-        a.href = `https://pro-s-portal-pr-2713.azurewebsites.net${url}`;
+        a.href = url;
         a.download = fileName + '.xlsx';
         document.body.appendChild(a);
         a.click();
