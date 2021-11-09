d2eefd41f64b254b3f40c7b7a76b2718ca522396
Joachim <joachim290@outlook.com>
2021-11-09 14:35:25 +0100

    chore: export table export handlers type

--- src/index.ts
+++ src/index.ts
@@ -166,7 +166,10 @@ export { PimsTask, ProCoSysTask } from './http/apiClients/models/fusionTasks';
 
 export { default as useApiClient } from './http/hooks/useApiClient';
 export { default as useApiClients } from './http/hooks/useApiClients';
-export { default as useExcelExport } from './http/hooks/dataExport/useExcelExport';
+export {
+    default as useExcelExport,
+    TableExportHandlers,
+} from './http/hooks/dataExport/useExcelExport';
 export {
     createPagination,
     applyPagination,
