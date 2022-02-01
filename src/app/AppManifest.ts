5c40cf81d016186bf8025e5909ff57f8fa7863c9
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2022-02-01 09:30:46 +0100

    fix: add render method to app manifest

--- src/app/AppManifest.ts
+++ src/app/AppManifest.ts
@@ -3,6 +3,8 @@ import { default as ApiAppManifest } from '../http/apiClients/models/fusion/apps
 
 type AppManifest = ApiAppManifest & {
     AppComponent: React.ComponentType;
+    // TODO - WIP: workaround while having fusion API and Fusion Framework
+    render?: (fusion: any, env: AppManifest) => React.LazyExoticComponent<React.ComponentType>;
 };
 
 export default AppManifest;
