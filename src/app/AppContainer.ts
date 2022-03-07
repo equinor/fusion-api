3de370076123d89edb61c683a2a8ce25802dcb94
AbdullZahir <Abdullah.zahir@hotmail.com>
2022-03-07 13:34:51 +0100

    fix: updated AppContainer to compare tags in compareApp function

--- src/app/AppContainer.ts
+++ src/app/AppContainer.ts
@@ -38,6 +38,18 @@ type ApiAppConfig<T> = {
     endpoints: Record<string, string>;
 };
 
+const compare  = (x:string[], y:string[]) => {
+    if (x.length === y.length) {
+        return false
+    }
+
+    for (const tag in x) {
+        if(!y.includes(tag)) {
+            return true;
+    }
+
+};
+
 const compareApp = (a: AppManifest, b?: AppManifest) => {
     if (!b) return true;
     const attr = Object.keys(a) as Array<keyof AppManifest>;
@@ -47,7 +59,7 @@ const compareApp = (a: AppManifest, b?: AppManifest) => {
             //@todo maybe?!?!
             case 'context':
             case 'tags':
-                return false;
+                return compare(a.tags, b.tags);
 
             case 'category':
                 return a.category?.id !== b.category?.id;
