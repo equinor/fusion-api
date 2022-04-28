ed3e645f0538a160c16456ff0b34acbeab189341
Eskil Lindløv Sand <eslsa@equinor.com>
2022-04-28 15:39:32 +0200

    removed type

--- src/http/apiClients/models/org/Organisation.ts
+++ src/http/apiClients/models/org/Organisation.ts
@@ -3,7 +3,6 @@ import Position from './Position';
 export type Organisation = {
     id: string | null;
     name: string | null;
-    type: null;
     parentId: string | null;
     managerPosition: Position | null;
     managerPositionId: string | null;
